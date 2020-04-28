b19cdf453abf23030d22bf2a551ee8132588e55f
mrenadams <marissaa@mendwell.com>
2020-04-28 16:31:31 -0700

    onclick function

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ $(document).ready(function() {
     //console.log(moment().format('LL'))
     document.getElementById("currentDay").innerHTML = moment().format('LL');
     });
+
+//store tasks in local storage
+
+var taskInput = document.querySelector("#task");
+var saveButton = document.querySelector("#save-btn");
+
+
+saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("button works")
+  
+    var savedTask = document.querySelector("#task").value;
+    
+    localStorage.setItem("task", savedTask);
+    
+  });
+
+  
