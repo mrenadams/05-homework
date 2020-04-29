9be9673d720592b97519382e7a5e9fac8b617614
mrenadams <marissaa@mendwell.com>
2020-04-28 18:31:24 -0700

    working on local storage

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //console.log(moment().format('LL'))
     document.getElementById("currentDay").innerHTML = moment().format('LL');
-    });
+});
 
 //store tasks in local storage
 
 var taskInput = document.querySelector("#task");
-var saveButton = document.querySelector("#save-btn");
+var saveButton = document.querySelectorAll(".btn-primary");
 
 
-saveButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    console.log("button works")
-  
-    var savedTask = document.querySelector("#task").value;
-    
-    localStorage.setItem("task", savedTask);
-    
-  });
+saveButton.forEach(btnprimary => {
 
-  
+    btnprimary.addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log("button works")
+
+        var savedTask = document.querySelector("#task").value;
+        console.log(this)
+        console.log(savedTask)
+        localStorage.setItem("task", savedTask);
+
+    })
+
+
+});
+
